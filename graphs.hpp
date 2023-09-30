4a22da3cdc11904ac95f392a56e52f3ed67fd6f7
vazleo <vaz@poli.ufrj.br>
2023-09-30 20:03:33 -0300

    add diamater graph method

--- graphs.hpp
+++ graphs.hpp
@@ -186,6 +186,60 @@ class Graph {
         }
         output.close();
     }
+
+    int diameter(){
+        int diameter = 0;
+        bool infinite = 0;
+        
+        for (int i = 0; i < n; i++) {
+            vector<bool> visited(n, 0);
+            vector<int> level(n, -1);
+            queue<int> queue;
+
+            visited[i] = 1;
+            queue.push(i + 1);                // push root to queue
+            level[i] = 0;                       // level of the root is 0
+
+            while(!queue.empty()){
+                int current = queue.front();
+                queue.pop();
+
+                if(representation){                // check if graph is represented by matrix or vector
+                    for (int i = 0; i < adj_mx.m[current - 1].size(); i++) {
+                        if(adj_mx.m[current - 1][i] && !visited[i]){
+                            visited[i] = 1;
+                            if(level[current - 1] + 1 > diameter)
+                                diameter = level[current - 1] + 1;
+                            level[i] = level[current - 1] + 1;
+                            queue.push(i + 1);
+                        }
+                    }
+                }
+                else{
+                    for (int i = 0; i < adj_v.v[current - 1].size(); i++) {
+                        if(!visited[adj_v.v[current - 1][i] - 1]){
+                            visited[adj_v.v[current - 1][i] - 1] = 1;
+                            if(level[current - 1] + 1 > diameter)
+                                diameter = level[current - 1] + 1;
+                            level[adj_v.v[current - 1][i] - 1] = level[current - 1] + 1;
+                            queue.push(adj_v.v[current - 1][i]);
+                        }
+                    }
+                }
+            }
+            
+            if(i == 0){
+                for (int i = 0; i < visited.size(); i++) {
+                    if(!visited[i])
+                        infinite = 1;
+                }
+            }
+            if(infinite)
+                break;
+        }
+
+        return infinite ? -1 : diameter;
+    }
 };
 
 /*
